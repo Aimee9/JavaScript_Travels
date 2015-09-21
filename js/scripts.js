7ff95018ce9a94732118838a1af8a870a958540d
aimee reiss & yvonne Peng <student@epicodus.com>
2015-09-21 14:27:13 -0700

    Add google maps API

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
+var geocoder;
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(45.542, -122.654);
+  var mapOptions = {
+    zoom: 3,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+function codeAddress() {
+  var address = document.getElementById("new-country-city").value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+
 
 
 $(document).ready(function() {
+
   $("#add-attraction").click(function() {
     $("#new-attractions").append('<div class="new-attraction">' +
                                  '<div class="form-group">' +
@@ -27,6 +55,7 @@ $(document).ready(function() {
 
   var newPlace = { place: inputtedPlace, attractions: [] };
 
+
   $(".new-attraction").each(function() {
     var inputtedLandmark = $(this).find("input.new-landmark").val();
     var inputtedDate = $(this).find("input.new-date").val();
@@ -34,26 +63,31 @@ $(document).ready(function() {
 
     var newAttraction = { landmark: inputtedLandmark, date: inputtedDate, notes: inputtedNotes };
     newPlace.attractions.push(newAttraction);
-  });
-
+  }); // end of new-attraction.each function
 
   $("ul#places").append("<li><span class='place'>" + newPlace.place + "</span></li>");
 
-  $(".place").last().click(function() {
-    $("#show-places").show();
+    $(".place").last().click(function() {
+      $("#show-places").show();
 
-    $("#show-places h2").text(newPlace.place);
+      $("#show-places h2").text(newPlace.place);
 
-    $("ul#attractions").text("");
-    newPlace.attractions.forEach(function(attraction) {
-      $("ul#attractions").append("<li>" + attraction.landmark + ", " + attraction.date + ", " + attraction.notes + "</li>");
-    });
-  });
+      $("ul#attractions").text("");
 
-  $("input#new-country-city").val("");
-  $("input.new-landmark").val("");
-  $("input.new-date").val("");
-  $("input.new-notes").val("");
-}); // end of new-contact submit
+      newPlace.attractions.forEach(function(attraction) {
+
+        $("ul#attractions").append("<li> Landmark:  " + attraction.landmark + "<br>Date:  " + attraction.date + "<br>Note:  " + attraction.notes + "</li>");
+
+      }); // end of newPlace.attractions.forEach
+
+    $("input#new-country-city").val("");
+    $("input.new-landmark").val("");
+    $("input.new-date").val("");
+    $("input.new-notes").val("");
+
+    }); // end of places last() click
+
+}); // end of new-places submit
+
 
 }); // end of document
